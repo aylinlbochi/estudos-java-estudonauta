e4e8a6592f7a754a3bcfe6f03a1c62295b6f3e29
Aylin <aylinbochi@outlook.com>
2026-04-08 17:52:27 -0300

    Exercícios 36 e 37

---
+++ Exercicio36.java
@@ -0,0 +1,40 @@
+/*  Faça um programa que receba o sálario atual de um funcionário, 
+calcule e mostre o valor do aumento e o novo salário.
+Salário               Percentual de aumento
+Até R$300,00                     15%
+R$300 -> R$600,00                10%
+R$600-> R$900,00                 5%
+Acima de R$900,00                0% 
+*/
+import java.util.Scanner;
+public class Exercicio36 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double salario, aumento, novo_salario;
+        System.out.print("Qual o valor do sálario do funcionário? R$ ");
+        salario = sc.nextDouble();
+
+        System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+        if(salario <= 300){
+            aumento = salario * 0.15;
+            novo_salario = salario + aumento;
+            System.out.printf("O valor do aumento foi de R$ %.2f", aumento);
+            System.out.printf("\nO valor do novo salário é de R$ %.2f", novo_salario);
+        }else if(salario <= 600){
+            aumento = salario * 0.10;
+            novo_salario = salario + aumento;
+            System.out.printf("O valor do aumento foi de R$ %.2f", aumento);
+            System.out.printf("\nO valor do novo salário é de R$ %.2f", novo_salario);
+        }else if(salario <= 900){
+            aumento = salario * 0.05;
+            novo_salario = salario + aumento;
+            System.out.printf("O valor do aumento foi de R$ %.2f", aumento);
+            System.out.printf("\nO valor do novo salário é de R$ %.2f", novo_salario);
+        }else{
+            System.out.println("Esse funcionário não tem direito a aumento");
+        }
+        System.out.println("\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+        sc.close();
+    }
+}
---
+++ Exercicio37.java
@@ -0,0 +1,36 @@
+/*Faça um programa que receba o salário bruto de um funcionário,
+calcule e mostre o valor a receber. Sabe-se que este é composto
+pelo salário bruto acrescido de gratificação e descontando
+o imposto de 7% sobre o salário.
+Salário                     Gratificação
+Até R$ 350,00                 R$ 100,00
+R$ 350,00 -> R$ 600,00        R$ 75,00
+R$ 600,00 -> R$ 900,00        R$ 50,00
+Acima de R$ 900,00               R$ 35,00
+ */
+import java.util.Scanner;
+public class Exercicio37 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        double salario, imposto, novo_salario;
+        System.out.print("Informe o valor do salário bruto do funcionário: R$ ");
+        salario = sc.nextDouble();
+
+        System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+        imposto = salario * 0.07;
+        if(salario <= 350){
+            novo_salario = (salario - imposto) + 100;
+        }else if (salario <= 600){
+            novo_salario = (salario - imposto) + 75;
+            
+        }else if(salario <= 900){
+            novo_salario = (salario - imposto) + 50;
+        }else{
+            novo_salario = (salario - imposto) + 35;
+        }
+        System.out.printf("\nO funcionário irá receber R$ %.2f", novo_salario);
+        System.out.println("\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+        sc.close();
+    }
+}
